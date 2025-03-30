a2c7fcaafc4c47be9717c0c1b9f27da1fe5e3842
Daniel <callejasmark63@gmail.com>
2025-03-30 22:32:40 +0800

    ❄️ Added book endpoints

--- expensetrackerAPI/urls.py
+++ expensetrackerAPI/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('books/', views.BooksView.as_view()),
+    path('books/<int:pk>', views.SingleBookView.as_view()),
 ]
--- expensetrackerAPI/views.py
+++ expensetrackerAPI/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# * Django Rest Framework 🔥
+from rest_framework import generics
+
+# * Models 🗂️
+from .models import Category, Book
+from .serializers import CategorySerializer, BookSerializer
+
 # Create your views here.
 def home(request):
     return HttpResponse('Welcome to Expense Tracker APIs!')
+
+# * READ/WRITE @ ALL BOOKS
+class BooksView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # * GET ✅
+    # * POST ✅
+
+# * GET/UPDATE/DELETE
+class SingleBookView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
