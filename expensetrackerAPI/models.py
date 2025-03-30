706e867ea8030cc754f64bb4613693369e962de2
Daniel <callejasmark63@gmail.com>
2025-03-30 22:00:35 +0800

    ❄️ Initial Commit, Models added

---
+++ expensetrackerAPI/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+    
+    class Meta:
+        verbose_name_plural = "Category"
+
+class Book(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    authors = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    published_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    distribution_expense = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+    
