65a949cec21af4bd5b1dad1630a713323510553c
Daniel <callejasmark63@gmail.com>
2025-03-31 22:09:03 +0800

    ❄️ Added pagination

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -132,6 +132,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     )
--- expensetrackerAPI/views.py
+++ expensetrackerAPI/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.db.models import Sum
 # * Django Rest Framework 🔥
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 # * Models 🗂️
 from .models import Category, Book
 from .serializers import CategorySerializer, BookSerializer, UserRoleSerializer
 
-# Create your views here.
+# * Pagination
+class CustomPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_description = 'page_size'
+    max_page_size = 100
+
 def home(request):
     return HttpResponse('Welcome to Expense Tracker APIs!')
 
@@ -17,8 +25,8 @@ def home(request):
 class BooksView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPagination
     # * GET ✅
     # * POST ✅
 
@@ -27,6 +35,12 @@ class SingleBookView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# * GET total expenses
+class TotalExpensesView(APIView):
+    def get(self, request):
+        total_expense = Book.objects.aggregate(total=Sum('distribution_expense'))['total'] or 0
+        return Response({"total_expenses": total_expense})
 
 # * Admin will assign user to group "editors"
 class UserGroupUpdateView(generics.UpdateAPIView):
