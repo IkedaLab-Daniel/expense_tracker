306aa4094024bab918dcfd95a6d9b0de75740aeb
Daniel <callejasmark63@gmail.com>
2025-04-06 00:21:43 +0800

    ❄️ Added CORS

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -149,3 +151,6 @@ REST_FRAMEWORK = {
     )
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # your React dev server
+]
